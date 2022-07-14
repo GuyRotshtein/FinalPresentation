3a99e4c954781913843f226e978b7533d3ae62dc
omarii20 <mohamedomarii44301@gmail.com>
2022-07-14 03:59:04 +0300

    get list of owners of specific pet

--- objectPage.php
+++ objectPage.php
@@ -10,10 +10,25 @@ if(!isset($_SESSION["owner_id"])){header('Location: index.php');}
 if(!isset($_GET['pet_id'])){header('Location: listPage.php');}
 $id = $_GET['pet_id'];
 
-$query = "SELECT * FROM dbShnkr22studWeb1.tbl_218_pet WHERE pet_id = " . $id;
+$query = "SELECT * 
+FROM dbShnkr22studWeb1.tbl_218_owner 
+RIGHT JOIN dbShnkr22studWeb1.tbl_218_owners_pets
+ON dbShnkr22studWeb1.tbl_218_owner.owner_id = dbShnkr22studWeb1.tbl_218_owners_pets.owner_id
+AND dbShnkr22studWeb1.tbl_218_owners_pets.pet_id =" . $id;
+
+$query1 = "SELECT * FROM dbShnkr22studWeb1.tbl_218_pet WHERE dbShnkr22studWeb1.tbl_218_pet.pet_id =" . $id;
+
 $result = mysqli_query($connection, $query);
-$row = mysqli_fetch_array($result);
+$result1 = mysqli_query($connection, $query1);
+
+$row1 = mysqli_fetch_array($result1);
+
+if(!$result){
+    die("DB query faild!");
+}
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -54,7 +69,7 @@ $row = mysqli_fetch_array($result);
     <nav class="breadCrumbs">
         <a href="./homePage.php" class="firstBreadCrumb">Home</a>
         <a href="./listPage.php" class="BreadCrumb">My Pets</a>
-        <a href="#" class="BreadCrumb">Sir Barkley</a>
+        <a href="#" class="BreadCrumb"><?php echo $row1["pet_name"];?></a>
         <a href="#" class="currentBreadCrumb">Pet Page</a>
     </nav>
 
@@ -89,9 +104,9 @@ $row = mysqli_fetch_array($result);
                 <input type="file">
             </div>
             <div>
-                <h1>Name: <span>Sir Barkley</span></h1>
-                <h5>Species: Mixed chiuaua</h5>
-                <h5>Age: 7 years old(Born 7.1.2015)</h5>
+                <h1>Name: <span><?php echo $row1["pet_name"];?></span></h1>
+                <h5>Species: <?php echo $row1["species"];?></h5>
+                <h5>Age: <?php echo $row1["age"];?></h5>
             </div>
         </div>
         <!--  Owners list  -->
@@ -101,38 +116,19 @@ $row = mysqli_fetch_array($result);
                 <a href="#">Edit List</a>
             </div>
             <div class="ownersList">
-                <div class="owner">
-                    <h4>Greg Buskilla</h4>
-                    <h3>Details</h3>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                <?php
+                while($row = mysqli_fetch_array($result))
+                {
+                    echo '<div class="owner">';
+                    echo '<h4>'.$row["name"].'</h4>';
+                    echo '<h3>Details</h3>';
+                    echo '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle details" viewBox="0 0 16 16">
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                         <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                    </svg>
-                </div>
-                <div class="owner">
-                    <h4>Dio Brando</h4>
-                    <h3>Details</h3>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                    </svg>
-                </div>
-                <div class="owner">
-                    <h4>Owen Richardson</h4>
-                    <h3>Details</h3>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                    </svg>
-                </div>
-                <div class="owner">
-                    <h4>Your Mom</h4>
-                    <h3>Details</h3>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
-                        <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
-                    </svg>
-                </div>
+                    </svg>';
+                    echo '</div>';
+                }
+                ?>
             </div>
         </div>
 
